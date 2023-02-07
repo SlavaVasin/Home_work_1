edfc2b5237f8aa10aba44734debd9b6cf300cc5a
Vasin Viacheslav <in-lite@list.ru>
2023-02-07 14:57:48 +0500

    Task 20

--- Home_Work_3.py
+++ Home_Work_3.py
@@ -59,3 +59,27 @@
 ноутбук
 Вывод:
 12'''
+#  Сделал чтобы считал и английский и русские буквы вместе
+languages_en = {
+    1: 'A, E, I, O, U, L, N, S, T, R',
+    2: 'D, G', 3: 'B, C, M, P', 4: 'F, H, V, W, Y',
+    5: 'K', 8: 'J, X', 10: 'Q, Z',
+}
+languages_ru = {
+    1: 'А, В, Е, И, Н, О, Р, С, Т',
+    2: 'Д, К, Л, М, П, У', 3: 'Б, Г, Ё, Ь, Я',
+    4: 'Й, Ы', 5: 'Ж, З, Х, Ц, Ч',
+    8: 'Ш, Э, Ю', 10: 'Ф, Щ, Ъ'
+}
+word = input('Введите слово: ').upper()
+count = 0
+for i in word:
+    for j in languages_en:
+        if i in languages_en[j]:
+            count = count + j
+
+for i in word:
+    for j in languages_ru:
+        if i in languages_ru[j]:
+            count = count + j
+print(count)
